3859954b0d972836b3f62e7420d7888ebfeff1c5
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-01-01 23:02:36 +0900

    done

---
+++ easy/perfectNumber.js
@@ -0,0 +1,8 @@
+var checkPerfectNumber = function (num, sum = 0) {
+  for (let i = 1; i <= Math.trunc(num / 2); i++) {
+    num % i === 0 && (sum += i);
+  }
+  return num === sum
+};
+
+console.log(checkPerfectNumber(28));
