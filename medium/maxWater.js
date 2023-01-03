ebc4249daf1ddd9b74d98b33a6106c403be1a3ae
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-01-03 10:37:41 +0900

    maxWater

---
+++ medium/maxWater.js
@@ -0,0 +1,16 @@
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let max_area = 0;
+  while (left < right) {
+    area = Math.min(height[left], height[right]) * (right - left);
+    max_area = Math.max(max_area, area)
+    if (height[left] < height[right]) {
+      left += 1;
+    } else {
+      right -= 1;
+    }
+  }
+  return max_area
+};
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
