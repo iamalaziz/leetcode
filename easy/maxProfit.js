db648c5f24bced51bd74633b285abeaf67c00996
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-01-06 23:10:19 +0900

    5 problems done

---
+++ easy/maxProfit.js
@@ -0,0 +1,13 @@
+var maxProfit = function (prices) {
+  let buy = prices[0];
+  let profit = 0;
+  for (i = 1; i < prices.length; i++) {
+    if (buy > prices[i]) {
+      buy = prices[i];
+    } else {
+      profit = Math.max(profit, prices[i] - buy);
+    }
+  }
+  return profit;
+};
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
---
+++ easy/reformat.js
@@ -0,0 +1,32 @@
+function reformat(s) {
+  let letters = "";
+  let digits = "";
+
+  for (const ch of s) {
+    if (ch.match(/[a-z]/i)) {
+      letters += ch;
+    } else {
+      digits += ch;
+    }
+  }
+  if (Math.abs(letters.length - digits.length) > 1) {
+    return "";
+  }
+
+  let isLetter = false;
+  if (letters.length - digits.length == 1) isLetter = true;
+  let result = "";
+  for(i = 0; i< s.length; i++) {
+    if(isLetter) {
+      result += letters[0]
+      letters = letters.substring(1)
+    }else {
+      result += digits[0]
+      digits = digits.substring(1)
+    }
+    isLetter = !isLetter
+  }
+
+  return result;
+}
+console.log(reformat("covid2019"));
