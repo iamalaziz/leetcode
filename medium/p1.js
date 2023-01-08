016406d770ecaefc46525a4de9856bbce04eef7f
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-01-08 21:55:01 +0900

    327 contest problem

---
+++ medium/p1.js
@@ -0,0 +1,7 @@
+var maxKelements = function (nums, k) {
+  for (let i = 0; i < k; i++) {
+    let select = Math.floor(Math.random() * (nums.length - 1));
+    
+  }
+};
+console.log(maxKelements([1, 10, 3, 3, 3], 3));
