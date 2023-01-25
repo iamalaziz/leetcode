234e2ae2026d0a8ea69b330a78a27ce77e12340b
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-01-25 23:36:44 +0900

    3 problems

---
+++ easy/consistentStrings.js
@@ -0,0 +1,5 @@
+var countConsistentStrings = function (allowed, words) {
+  return words.reduce((a, w) => a + +w.split('').every(c => allowed.indexOf(c) > -1), 0)
+
+}
+console.log(countConsistentStrings('ab', ['ad', 'bd', 'aaab', 'baa', 'badab']));
---
+++ easy/smallerThanCurrent.js
@@ -0,0 +1,11 @@
+var smallerNumbersThanCurrent = function (nums) {
+  return nums.map((num) => {
+    let count = 0;
+    nums.forEach((n) => {
+      if (num > n) count++;
+    });
+    return count;
+  });
+};
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
